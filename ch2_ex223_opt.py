63bbf94786afbe98bbd5618e6c152dafd47102dc
Yiwen <35602732+Yiwen-Lu@users.noreply.github.com>
2018-09-14 18:25:57 +0200

    Add files via upload

---
+++ ch2_ex223_opt.py
@@ -0,0 +1,36 @@
+# Exercise 2.2.3 optimal version
+
+def convert_time(minutes, seconds):
+	#ss = seconds % 60
+	mm = minutes + seconds // 60
+	hh = minutes // 60
+	return hh, mm
+
+
+def back_time(start_time, speeds, miles):
+	start_hh, start_mm = str(start_time).split(':')
+	total_min, total_sec = 0, 0
+
+	for v,s in zip(speeds, miles):
+		minute, second = str(v).split(':')
+		min_spent = s * int(minute)
+		sec_spent = s * int(second)
+		total_min += min_spent
+		total_sec += sec_spent
+
+	hh, mm = convert_time(total_min, total_sec)
+	back_hh = int(start_hh) + hh
+	back_mm = int(start_mm) + mm
+	if back_mm >= 60:
+		back_hh += 1
+		back_mm -= 60
+
+	return str(int(back_hh)) + ":" + str(int(back_mm))
+
+
+if __name__ == '__main__':
+	speeds = ['8:15', '7:12', '8:15']
+	miles = [1, 3, 1]
+	start_time = '6:52'
+	breakfast_time = back_time(start_time, speeds, miles)
+	print("At %s I get home for breakfast." % breakfast_time)
