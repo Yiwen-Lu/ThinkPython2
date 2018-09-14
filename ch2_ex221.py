f4f94970b4113eb711156647d746e7307864de3d
Yiwen <35602732+Yiwen-Lu@users.noreply.github.com>
2018-09-14 15:55:48 +0200

    Update and rename ch2.py to ch2_ex221.py

---
+++ ch2_ex221.py
@@ -0,0 +1,9 @@
+# Exercise 2.2.1
+import math
+
+def volume(r):
+	return 4/3 * math.pie * r**3
+
+if __name__ == "__main__":
+    radius = 5
+    print("the volume of a sphere with radius 5 is {.4f}".format(volume(radius)))
